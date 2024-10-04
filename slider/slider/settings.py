c7aeab99e91cc9f035db6e965d138ac681e3f76e
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2024-10-04 10:27:17 +0300

    Create custom user

--- slider/slider/settings.py
+++ slider/slider/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "users.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
