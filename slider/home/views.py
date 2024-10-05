7ac083186192c16c04d128a4ee3eb8863db724d1
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2024-10-05 11:56:32 +0300

    Add homepage application and templates

---
+++ slider/home/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+def home(request: HttpRequest) -> HttpResponse:
+    return render(request, 'home/home.html')
--- slider/slider/settings.py
+++ slider/slider/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'slider.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
