fb56e7a7c52e313ef7fb9fc72c49c3f019adcab5
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2024-10-06 00:09:41 +0300

    Fix docker postgres volume | Add connection to postgres

--- slider/slider/settings.py
+++ slider/slider/settings.py
@@ -79,13 +79,23 @@ WSGI_APPLICATION = 'slider.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'slider',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
