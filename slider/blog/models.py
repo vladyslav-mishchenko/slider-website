f57c0b9fe59041749c0a255176cf437ceb3c02e5
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2024-10-07 23:39:38 +0300

    Add blog category and blog post models

--- slider/blog/models.py
+++ slider/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=256, null=True)
+    description = models.TextField(null=True)
+    text = models.TextField(null=True)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT, null=True)
