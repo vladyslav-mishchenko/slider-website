88f9078240bf6d90b4c77c61a0e1b25fb2b5ed35
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2024-10-23 22:30:48 +0300

    Add debug true

--- slider/slider/settings.py
+++ slider/slider/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mj0o_*y^6s^cgdqu860q6i4d%-2m-4fk_alfpip!ku4^j4wgc8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
