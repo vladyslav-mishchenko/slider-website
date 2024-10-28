48f4c034235c6d79df41c722fbd1ee42238632b2
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2024-10-28 11:50:02 +0200

    Add blog post markup and article image

--- slider/blog/models.py
+++ slider/blog/models.py
@@ -12,7 +12,8 @@ class Post(models.Model):
     title = models.CharField(max_length=256, null=True)
     description = models.TextField(null=True)
     text = models.TextField(null=True)
-    image_path = models.CharField(max_length=256, null=True)
+    img_preview = models.CharField(max_length=256, null=True)
+    img_article = models.CharField(max_length=256, null=True)
     post_slug = models.SlugField(max_length=128, unique=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.RESTRICT, null=True)
 
